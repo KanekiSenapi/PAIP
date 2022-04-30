da9112e432d64b7f229977d26943a489d17e7e0f
KanekiSenapi <vkanekisenpai@gmail.com>
2022-04-30 16:55:22 +0200

    PPP-2 Add router for url routing

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo phpinfo();
+require_once "Router.php";
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'DefaultController');
+
+Router::run($path);
