f723e9106f29df472168f54b15c3f246f11315fa
KanekiSenapi <vkanekisenpai@gmail.com>
2022-05-24 18:20:07 +0200

    PPP-9 Create header and footer elements with styles for mobile and pc

---
+++ public/js/global.js
@@ -0,0 +1,10 @@
+const navigationHamburgerButton = document.querySelector(".navigation-hamburger-button");
+const navigationMenu = document.querySelector(".navigation-menu");
+
+
+function handleHamburgerNavigation() {
+    navigationMenu.style.display = navigationMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+
+navigationHamburgerButton.addEventListener("click", handleHamburgerNavigation)
