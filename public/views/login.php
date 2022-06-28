7b2bf6db2bcee038da771615ce35c238163dd4d2
KanekiKen <vkanekisenpai@gmail.com>
2022-06-28 18:21:48 +0200

    Change fields types

--- public/views/login.php
+++ public/views/login.php
@@ -10,9 +10,9 @@
                     ?>
         </div>
         <label for="email-field">Email</label>
-            <input name="email" id="email-field" type="text" placeholder="Email">
+            <input name="email" id="email-field" type="email" placeholder="Email" required>
         <label for="password-field">Password</label>
-            <input name="password" id="password-field" type="password" placeholder="Password">
+            <input name="password" id="password-field" type="password" placeholder="Password" required>
         <button type="submit">LOGIN</button>
     </form>
 </div>
--- public/views/register.php
+++ public/views/register.php
@@ -10,15 +10,15 @@
                     ?>
         </div>
         <label for="email-label">Email</label>
-        <input name="email" id="email-label" type="text" placeholder="Email">
+        <input name="email" id="email-label" type="email" placeholder="Email" required>
         <label for="password-label">Password</label>
-        <input name="password" id="password-label" type="password" placeholder="Password">
+        <input name="password" id="password-label" type="password" placeholder="Password" required>
         <label for="password2-label">Repeat Password</label>
-        <input name="password2" id="password2-label" type="password" placeholder="Repeat Password">
+        <input name="password2" id="password2-label" type="password" placeholder="Repeat Password" required>
         <label for="name-label">Name</label>
-        <input name="name" id="name-label" type="text" placeholder="Name">
+        <input name="name" id="name-label" type="text" placeholder="Name" required>
         <label for="surname-label">Surname</label>
-        <input name="surname" id="surname-label" type="text" placeholder="Surname">
+        <input name="surname" id="surname-label" type="text" placeholder="Surname" required>
         <button type="submit">Register</button>
     </form>
 </div>
